f8fed384df9cc0d1f5b13b690186a3477cf8ff16
Johan Bothma <johan@whitehart.co.za>
2021-02-07 11:53:17 +0200

    Section 15

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -7,11 +7,12 @@ import 'firebase/storage';
 const firebaseConfig = {
     apiKey: "AIzaSyDa77Ld1tC6LvcsUsiuiGiwBgKjR5htqhc",
     authDomain: "re-vents-2bd17.firebaseapp.com",
+    databaseURL: "https://re-vents-2bd17-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "re-vents-2bd17",
     storageBucket: "re-vents-2bd17.appspot.com",
     messagingSenderId: "1086777375471",
     appId: "1:1086777375471:web:f19f918baa5df3936af91a"
-}
+};
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
